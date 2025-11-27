030faa2031738875a410beb4ffb0527506b075ed
kjkim <kjkim@osci.kr>
2025-11-27 15:43:16 +0900

    최초 커밋앤푸시

---
+++ src/main/java/com/example/benitback/BenitBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.benitback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenitBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BenitBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/benitback/BenitBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.benitback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenitBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
